3b7e22f6e34d3bcebbb5f42d34c447ca39814f27
Faïkidine AHMED <98451900+faikidine@users.noreply.github.com>
2025-01-17 16:14:03 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,26 @@
-ttt
+#include <stdio.h>
+#include <stdlib.h>
+#include "code_gen.h"
+
+extern FILE *yyin;
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <source file>\n", argv[0]);
+        return 1;
+    }
+
+    yyin = fopen(argv[1], "r");
+    if (!yyin) {
+        perror("failed to open file");
+        return 1;
+    }
+
+    init_code_generator("home/output.c");
+    yyparse();
+    close_code_generator();
+    fclose(yyin);
+
+    printf("compilation successful: home/output.c generated\n");
+    return 0;
+}
